5f61fe6d0b2fb9fbd8abe07ac9eefad825d739e3
euclidesseg <euder0206@gmail.com>
2023-02-25 11:40:52 -0500

    Operadores aritmeticos

---
+++ 03.OperadoresAritmeticos/Operadores.py
@@ -0,0 +1,20 @@
+print ("Suma")
+
+numeroUno = 9;
+numeroDos = 7;
+
+resultadoSuma = numeroUno + numeroDos;
+resultadoResta= numeroUno - numeroDos;
+resultadoMultiplicacion = numeroUno * numeroDos;
+resultadoDivision = numeroUno / numeroDos;
+
+print(f"El resultado de la Suma es: {resultadoSuma}")
+print(f"El resultado de la Resta es:{resultadoResta}")
+print(f"El resultado de la Multiplicacion es:{resultadoMultiplicacion}")
+print(f"El resultado de la Division es:{resultadoDivision}")
+
+#ESTOS SERIAN LOS OPERADORES MAS USADOS EN PYTHON PERO TAMBIEN TENEMOS LA POTENCIACION
+#ELEVAR DOS A LA 5 (MULTIPLICAR EL 2 POR SI MISMO 5 VECES)
+
+resultadoPotencia = numeroUno ** numeroDos;
+print(f"El resultado de la Potencia es:{resultadoPotencia}");
