41a54e043559a4f3914b88354d9ddd64f404db60
euclidesseg <euder0206@gmail.com>
2023-04-23 23:36:58 -0500

    encapsulacion y metodos accesores getter y setter

--- 19.POO/01.Clases/03.Encapsulacion.py
+++ 19.POO/01.Clases/03.Encapsulacion.py
@@ -7,28 +7,55 @@ __atributo
 
 class Servivo():
     # propiedades
-    tipo = ""
-    dieta = ""
-    area = ""
-    
     # metodos
     # este metodo sobrescribe las propiedades que ya estan
-    def __init__(self, nombre, tipo, dieta, area):
-        self.__tipo = tipo
-        self.__nombre = nombre
-        self.__dieta = dieta
-        self.__area = area        
-    
+    def __init__(self):        
+        self.__nombre = None
+        self.__dieta = None
+        self.__area = None        
+        self.__tipo = None
     # declarando metodos accesores
 
     # property indica que este datos va a ser uns propiedad encapsulada y que sera de retorno
     @property
-    def getNombre(self):
+    def nombre(self):
         return self.__nombre
+    @property
+    def dieta(self):
+        return self.__dieta
+    @property
+    def area(self):
+        return self.__area
+    @property
+    def tipo(self):
+        return self.__tipo
     
-    def getNombre(self, ):
-        self.__nombre = 
+    @nombre.setter
+    def nombre(self, nombre):
+        self.__nombre = nombre
+    @dieta.setter
+    def dieta(self, dieta):
+        self.__dieta = dieta
+    @area.setter
+    def area(self, area):
+        self.__area = area
+    @tipo.setter
+    def tipo(self, tipo):
+        self.__tipo = tipo
         
-servivo = Servivo("alchon", "ave", "roedores", "aire")
+        
+servivo = Servivo()
+servivo.nombre = "Alcon"
+servivo.dieta = "aves"
+servivo.area = "aire"
+servivo.tipo = "ave"
+print(servivo.nombre)
+print(servivo.dieta)
 print(servivo.tipo)
-print(servivo.get())
+print(servivo.area)
+
+    # por convencion de python los metodos setter y getter se suelen nombrear con el mismo 
+    # nombre de la propiedad
+    # y para el caso del set se le agrega el decorador property para indicar que va a retornar un valor 
+    # y para el setter el mismo nombre pero con el decorador de @nombredepropiedad.setter
+    
