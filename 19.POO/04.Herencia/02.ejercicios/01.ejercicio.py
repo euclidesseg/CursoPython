50f0ad18f695d3525fbb243dedc3ec32231d03d2
euclidesseg <euder0206@gmail.com>
2023-05-25 22:53:37 -0500

    Se crea ejercicio de herencia en python

--- 19.POO/04.Herencia/02.ejercicios/01.ejercicio.py
+++ 19.POO/04.Herencia/02.ejercicios/01.ejercicio.py
@@ -12,33 +12,57 @@ class Calculadora:
 
 
 
-class op_basicas(Calculadora):
+class op_basicas(Calculadora): # esta clase heredaria todos los metodos de Calculadora
     def __init__(self):
-        Calculadora.__init__(self, 2)
+        Calculadora.__init__(self, 2) # llamo al metodo constructor  de la clase padre y le mando como parametro 2 
+                                      # indicando que de ese tamañpo sera el arreglo 
 
-        def sumar(self valor1, valor2):
-           a,b, = self.datos
-           s = a + b
-           print('el resultado es: ', s)
+    def sumar(self):
+        a,b, = self.datos  # accedp al atributo datos de la clase hija que esta heredado de la clase Calculadora
+        s = a + b
+        print('el resultado es: ', s)
 
-           def restar(self valor1, valor2):
-           a,b, = self.datos
-           r = a - b
-           print('el resultado es: ', r)
+    def restar(self):
+        a,b, = self.datos  # accedp al atributo datos de la clase hija que esta heredado de la clase Calculadora
+        r = a - b
+        print('el resultado es: ', r)
 
-           def multiplicar(self valor1, valor2):
-           a,b, = self.datos
-           m = a + b
-           print('el resultado es: ', m)
+    def multiplicar(self):
+        a,b, = self.datos  # accedp al atributo datos de la clase hija que esta heredado de la clase Calculadora
+        m = a + b
+        print('el resultado es: ', m)
 
-           def dividir(self valor1, valor2):
-           a,b, = self.datos
-           d = a + b
-           print('el resultado es: ', d)
+    def dividir(self):
+        a,b, = self.datos  # accedp al atributo datos de la clase hija que esta heredado de la clase Calculadora
+        d = a + b
+        print('el resultado es: ', d)
+        
+# Explicacion:
+# Dentro de la línea a, b = self.datos, se está realizando una operación llamada "desempaquetado" o "asignación múltiple". 
+# En este caso, el arreglo self.datos tiene dos elementos y se espera que se asignen a las variables a y b en orden.
+# se debe poner coma despues de cada elemento que vallamos a usar para desempaquetar el arreglo
 
 class raiz(Calculadora):
     def __init__(self):
-        Calculadora.__init__(self,1)
+        Calculadora.__init__(self,1) # llamo al metodo de la clase padre y le mando como parametro 2
 
     
-    def
+    def raizCuadrada(self):
+        import math
+        a, = self.datos # debido a que el metodo
+        raiz = math.sqrt(a)
+        print(f'la raiz cuadrada de {1} es {raiz}')
+        
+    def raizCuvida(self):
+        import math
+        a, = self.datos
+        raizCuvica = math.pow(a, 1/3)
+        print(f'la raiz cuvica de {a} es {raizCuvica}')
+
+ejemplo = op_basicas()
+ejemplo.ingresarDatos()
+ejemplo.sumar()
+
+ejemplo2 = raiz()
+ejemplo2.ingresarDatos()
+ejemplo2.raizCuvida()
